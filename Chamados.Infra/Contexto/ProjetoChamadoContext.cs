756a94b1525211beb179fd56ea68032ed952beda
brunoog33 <brunoog33@gmail.com>
2019-02-14 20:06:02 -0200

    Adicionanto Entityframework e suas configurações

---
+++ Chamados.Infra/Contexto/ProjetoChamadoContext.cs
@@ -0,0 +1,60 @@
+﻿using Chamados.Domain.Entities;
+using Chamados.Infra.EntityConfig;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+
+namespace Chamados.Infra.Contexto
+{
+    public class ProjetoChamadoContext : DbContext
+    {
+        public ProjetoChamadoContext() : base("Chamados")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Chamado> Chamados { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>(); // Desabilita convenções do banco de dados
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties() // Toda coluna que possuir Id será chave da minha tabela
+                .Where(p => p.Name == p.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>() // Toda coluna que possuir for string será um varchar, pois como padrão pega nvarchar
+               .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>() // Toda coluna que possuir for string terá um tamanho específico, pois sempre pega max
+               .Configure(p => p.HasMaxLength(100));
+
+            modelBuilder.Configurations.Add(new ClienteConfiguration());
+
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                /*if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataModificacao").CurrentValue = DateTime.Now;
+                }*/
+            }
+
+            return base.SaveChanges();
+        }
+
+
+    }
+}
---
+++ Chamados.Infra/EntityConfig/ClienteConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Chamados.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chamados.Infra.EntityConfig
+{
+    public class ClienteConfiguration : EntityTypeConfiguration<Cliente>
+    {
+        public ClienteConfiguration()
+        {
+            HasKey(c => c.ClienteId);
+
+            Property(c => c.RazaoSocial)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            Property(c => c.NomeFantasia)
+                .IsRequired()
+                .HasMaxLength(200);
+        }
+    }
+}
